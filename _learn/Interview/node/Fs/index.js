128f6785cddd01aa10977b8cb096827004fe8c16
codytang <codytang@tencent.com>
2019-12-26 09:58:17 +0800

    node fs 学习

--- _learn/Interview/node/Fs/index.js
+++ _learn/Interview/node/Fs/index.js
@@ -1,44 +1,59 @@
 // 读取
 const fs = require('fs')
 
-fs.readFile('./1.txt', 'utf8', (err, data) => {
-  console.log(err, data)
-})
+// fs.readFile('./rename.txt', 'utf8', (err, data) => {
+//   console.log(err, data)
+// })
 
-const data = fs.readFileSync('./1.txt', 'utf8')
-console.log(data)
+// // const data = fs.readFileSync('./1.txt', 'utf8')
+// // console.log(data)
 
-// fs.createReadStream用到了我们前面介绍的events eventEmitter.on() 方法来监听事件
-const stream = fs.createReadStream('./1.txt', 'utf8')
-stream.on('data', (data) => console.log(data))
+// // fs.createReadStream用到了我们前面介绍的events eventEmitter.on() 方法来监听事件
+// const stream = fs.createReadStream('./1.txt', 'utf8')
+// stream.on('data', (data) => console.log(data))
 
-// 写入文件
+// // 写入文件
 
-fs.writeFile('./2.txt', Math.random(), 'utf8', (err, ) => {
-  console.log(err)
-})
+// fs.writeFile('./2.txt', Math.random(), 'utf8', (err, ) => {
+//   console.log(err)
+// })
 
-fs.writeFileSync('./3.txt', Math.random(), 'utf8')
+// fs.writeFileSync('./3.txt', Math.random(), 'utf8')
 
-const ws = fs.createWriteStream('./4.txt', 'utf8')
-ws.write(Math.random() + '')
-ws.end()
+// const ws = fs.createWriteStream('./4.txt', 'utf8')
+// ws.write(Math.random() + '')
+// ws.end()
 
-// ------
+// // ------
 
-// 删除文件
+// // 删除文件
 
-fs.unlink('./del-1.txt', err => console.log(err))
+// fs.unlink('./del-1.txt', err => console.log(err))
 
-// fs.unlinkSync('./del-2.txt')
+// // fs.unlinkSync('./del-2.txt')
 
-// 删除文件夹
+// // 删除文件夹
 
-fs.rmdir('./rmdir', err => console.log(err))
+// fs.rmdir('./rmdir', err => console.log(err))
 
-// fs.rmdirSync('./rmdir')
+// // fs.rmdirSync('./rmdir')
 
-// 新建文件夹
+// // 新建文件夹
 
-fs.mkdir('./mkdir', err => console.log(err))
-// fs.mkdirSync('./mkdir-2')
+// fs.mkdir('./mkdir', err => console.log(err))
+// // fs.mkdirSync('./mkdir-2')
+
+// // 重命名 文件/文件夹
+
+// // fs.renameSync('./1.txt', './rename.txt')
+// fs.renameSync('./mkdir','./rename')
+
+// 复制文件 文件夹
+fs.copyFileSync('./2.txt', './copy-file.txt')
+
+// 下面这个报错
+// fs.copyFileSync('./rename', 'copy')
+
+
+const stat = fs.statSync('./copy-file.txt')
+console.log(stat.isDirectory(), stat.isFile())
--- _learn/Interview/node/README.md
+++ _learn/Interview/node/README.md
@@ -1,1 +1,3 @@
 https://juejin.im/post/5e0006c251882512795675f9?utm_source=gold_browser_extension#1
+
+https://github.com/jprichardson/node-fs-extra 操作文件 fs
